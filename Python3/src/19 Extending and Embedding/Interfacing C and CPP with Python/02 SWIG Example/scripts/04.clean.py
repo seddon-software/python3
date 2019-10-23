23bc304fdb569f3209a21e0ffab69bb3e6ddce87
Chris Seddon <seddon-software@keme.co.uk>
2019-10-23 22:13:16 +0100

    minor changes

--- Python3/src/19 Extending and Embedding/Interfacing C and CPP with Python/02 SWIG Example/scripts/04.clean.py
+++ Python3/src/19 Extending and Embedding/Interfacing C and CPP with Python/02 SWIG Example/scripts/04.clean.py
@@ -2,6 +2,7 @@ import os
 
 os.chdir("../src")
 os.system("python setup.py clean --all")
+os.system("rm hello_wrap.c")
 print("staging area cleaned")
 
 
--- Python3/src/19 Extending and Embedding/Interfacing C and CPP with Python/03 CPP SWIG Example/scripts/04.clean.py
+++ Python3/src/19 Extending and Embedding/Interfacing C and CPP with Python/03 CPP SWIG Example/scripts/04.clean.py
@@ -2,6 +2,7 @@ import os
 
 os.chdir("../src")
 os.system("python setup.py clean --all")
+os.system("rm myexample_wrap.cpp")
 print("staging area cleaned")
 
 
